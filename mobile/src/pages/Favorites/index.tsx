e795e592f505dd4aae194957aa83b093450faf1f
Ycaro-tecnologia <ycarosilva87@gmail.com>
2020-08-12 16:09:27 -0300

    pages favorites

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,82 @@
+/*import React from 'react';
+import { View, ScrollView } from 'react-native';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+import styles from './styles';
+
+function Favorites() {
+    return( 
+    <View style={styles.container}>
+        <PageHeader title="Meus Proffys Favoritos " />
+
+        <ScrollView
+         style={styles.teacherList}
+         contentContainerStyle={{
+             paddingHorizontal: 16,
+             paddingBottom: 16,
+         }}
+         >   
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+        </ScrollView>
+
+    </View>
+    )
+}
+
+export default Favorites;
+*/
+
+import React, { useCallback, useState, useEffect } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
+import styles from './styles';
+
+const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  const loadFavorites = useCallback(() => {
+    AsyncStorage.getItem('favorites').then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }, []);
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {favorites.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} favorited />;
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Favorites;
